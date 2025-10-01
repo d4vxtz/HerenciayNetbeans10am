b90b2f3b5acc86effd593e77b24ad5dc6ea545df
juan.villegas_s <juan.villegas_s@L137494.uao.edu.co>
2025-10-01 10:23:55 -0500

    este es mi commit de las 10am

---
+++ src/main/java/com/mycompany/herenciaynetbeans10am/HerenciayNetbeans10am.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.herenciaynetbeans10am;
+
+/**
+ *
+ * @author juan.villegas_s
+ */
+public class HerenciayNetbeans10am {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
